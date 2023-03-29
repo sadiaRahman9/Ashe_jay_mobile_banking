3465561c3e863f5908bbc8022be4bee6651e9f47
sgitahub <srahman192025@bscse.uiu.ac.bd>
2023-03-30 00:07:52 +0600

    updated staff files

--- src/StuffUser/AfterCreate.java
+++ src/StuffUser/AfterCreate.java
@@ -11,6 +11,13 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
--- src/StuffUser/AfterDepositMoney.java
+++ src/StuffUser/AfterDepositMoney.java
@@ -10,6 +10,12 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
